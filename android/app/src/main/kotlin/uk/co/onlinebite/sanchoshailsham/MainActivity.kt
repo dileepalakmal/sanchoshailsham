3eb006b231ee5ac37566957727a2311e19a39bd4
Dileepa Bandara <76730578+dileepalakmal@users.noreply.github.com>
2022-06-06 13:13:43 +0530

    Add files

---
+++ android/app/src/main/kotlin/uk/co/onlinebite/sanchoshailsham/MainActivity.kt
@@ -0,0 +1,6 @@
+package uk.co.onlinebite.sanchoshailsham
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/theme/about_app.dart
@@ -0,0 +1,31 @@
+class AboutApp {
+  static const String appName = "Sanchos Hailsham";
+  static const String appCopyright =
+      "©2022 | All rights reserved | onlinebite.co.uk";
+  static const String appLocation =
+      "Sanchos 43 High Street, Hailsham,\nBN27 1AN";
+  static const String appWebsite = "https://www.sanchoshailsham.co.uk";
+  static const String appEmail = "info@sanchoshailsham.co.uk";
+  static const String appPhone = "01323847500";
+
+  static const String urlFacebook = 'https://facebook.com';
+  static const String urlWhatsApp = 'https://whatsapp.com';
+  static const String urlInstagram = 'https://instagram.com';
+  static const String urlYouTube = 'https://youtube.com';
+  static const String urlOnlineBite = 'https://onlinebite.co.uk';
+}
+
+// Icons
+
+// <a href="https://www.flaticon.com/free-icons/order" title="order icons">Order icons created by Yoteyo - Flaticon</a>
+// <a href="https://www.flaticon.com/free-icons/meal" title="meal icons">Meal icons created by Freepik - Flaticon</a>
+// <a href="https://www.flaticon.com/free-icons/restaurant" title="restaurant icons">Restaurant icons created by Freepik - Flaticon</a>
+// <a href="https://www.flaticon.com/free-icons/contact" title="contact icons">Contact icons created by Freepik - Flaticon</a>
+
+// <a href="https://www.flaticon.com/free-icons/pin" title="pin icons">Pin icons created by Freepik - Flaticon</a>
+// <a href="https://www.flaticon.com/free-icons/www" title="www icons">Www icons created by Freepik - Flaticon</a>
+// <a href="https://www.flaticon.com/free-icons/email" title="email icons">Email icons created by Freepik - Flaticon</a>
+// <a href="https://www.flaticon.com/free-icons/phone" title="phone icons">Phone icons created by Freepik - Flaticon</a>
+
+// Images
+// <a href="https://www.flaticon.com/free-stickers/food-delivery" title="food delivery stickers">Food delivery stickers created by inipagistudio - Flaticon</a>
